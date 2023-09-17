a588878ab195f7a7944c630d3f16d915f8cf3c10
Felipe Holanda <felipe.nether@icloud.com>
2023-09-17 01:09:32 -0300

    fix: wrong env

--- backend/src/data-source.js
+++ backend/src/data-source.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 async function connect() {
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.DB_URL);
         console.log("[INFO]: Conectado ao banco de dados");
     }catch(err){
         console.log("[ERRO]: Banco não conectado, encerrando aplicação.");
+        process.exit(1);
     }
 }
 
